b7307c9c76af2e96b183b1d97ead5990835c36cb
David <161796631+CodeofGuff@users.noreply.github.com>
2024-12-02 01:54:22 -0800

    Update state.js

--- js/state.js
+++ js/state.js
@@ -33,7 +33,7 @@ export const state = {
     lineWidth: 5,
     lastX: 0,
     lastY: 0,
-    textSize: 26,
+    textSize: 28,
     lastUsedMarker: null,
     isDrawingStraightLine: false,
     straightLineStart: null,
