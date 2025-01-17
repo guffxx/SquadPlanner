f423bf91c0afa2e93c7581b9d490fb99734aaee6
David <161796631+CodeofGuff@users.noreply.github.com>
2025-01-17 01:25:38 -0800

    increase text size for input

--- js/state.js
+++ js/state.js
@@ -33,7 +33,7 @@ export const state = {
     lineWidth: 5,
     lastX: 0,
     lastY: 0,
-    textSize: 28,
+    textSize: 36,
     lastUsedMarker: null,
     isDrawingStraightLine: false,
     straightLineStart: null,
